aaf205a07f94c286da301f803885d325f1614cf1
globopedia <globopediaoficial@gmail.com>
2026-04-07 05:26:44 -0300

    Update data.js

--- api/data.js
+++ api/data.js
@@ -5,8 +5,8 @@ export default async function handler(req, res) {
   const archivos = {
     lubs: { repo: "Lubs", path: "Ivals.csv" },
     utbolists: { repo: "Utbolists", path: "Utbolists.csv" },
-    rbits: { repo: "Rbits", path: "Rbits.csv" },
-
+    rbits: { repo: "Artids", path: "Rbits.csv" },
+    ankg: { repo: "Artids", path: "Ankg.csv" },
     artids1940: { repo: "Artids", path: "Artids1940.csv" },
     artids1955: { repo: "Artids", path: "Artids1955.csv" },
     artids1970: { repo: "Artids", path: "Artids1970.csv" },
