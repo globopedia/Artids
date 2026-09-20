1dfba82f77b9039ef43653460706ed74eaf4ae13
globopedia <globopediaoficial@gmail.com>
2026-09-20 02:54:15 -0300

    Rename CSV files for consistency

--- api/data.js
+++ api/data.js
@@ -4,9 +4,9 @@ export default async function handler(req, res) {
 
   const archivos = {
     lubs: { repo: "Lubs", path: "Ivals.csv" },
-    utbolistsa: { repo: "Lubs", path: "Utbolistsa.csv" },
-    utbolistse: { repo: "Lubs", path: "Utbolistse.csv" },
-    utbolistso: { repo: "Lubs", path: "Utbolistso.csv" },
+    utbolistsa: { repo: "Lubs", path: "UtbolistsA.csv" },
+    utbolistse: { repo: "Lubs", path: "UtbolistsE.csv" },
+    utbolistso: { repo: "Lubs", path: "UtbolistsO.csv" },
     orneos: { repo: "Lubs", path: "Orneos.csv" },
     rbits: { repo: "Lubs", path: "Rbits.csv" },
     ankg: { repo: "Artids", path: "Ankg.csv" },
